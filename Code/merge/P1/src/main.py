07d21596ef24a9fb45128666b1c70c74e025c9e9
THECODER-01 <noljunk100@gmail.com>
2026-01-23 17:44:05 +0000

    On branch Nolan Made a new folder for *P1* code  Changes to be committed: 	new file:   Code/merge/P1/.vscode/extensions.json 	new file:   Code/merge/P1/.vscode/settings.json 	new file:   Code/merge/P1/.vscode/vex_project_settings.json 	new file:   Code/merge/P1/src/main.py

---
+++ Code/merge/P1/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       codespace                                                    #
+# 	Created:      1/23/2026, 5:43:40 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
