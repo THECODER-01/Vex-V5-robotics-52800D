1942a5c1a87f72fbc8827afd759a04962f08c6f6
THECODER-01 <noljunk100@gmail.com>
2026-02-05 17:02:31 -0700

    modified:   Code/test/src/test2/src/main.py

--- Code/test/src/test2/src/main.py
+++ Code/test/src/test2/src/main.py
@@ -86,7 +86,7 @@ bumper_a = Bumper(brain.three_wire_port.a)
 # add something here
 
 # 1:400, 2:756, 3:250
-auto_at_start: false
+auto_at_start = False
 F1 = 400
 F2 = 756
 F3 = 250
@@ -106,30 +106,30 @@ place_Holder = Event()
 
 def Automonus_callback_0():
     global Automonus, Bottom, Top, O12B, O12F, O12S, AStop, PH, PL, place_Holder, Keep_Code
-    if auto_at_start true:
+    if auto_at_start == False:
         pass
-    if bumper_a.pressing():
-        # Right side
-        drivetrain.drive_for(FORWARD, F1, MM)
-        drivetrain.turn_for(RIGHT, 90, DEGREES)
-        drivetrain.drive_for(FORWARD, F2, MM)
-        drivetrain.turn_for(LEFT, 90, DEGREES)
-        drivetrain.drive_for(FORWARD, F3, MM, wait=False)
-        AUTOP.broadcast()
-        # Intake on
-        # Top goal
-        wait(8, SECONDS)
-    else:
-        # Left side
-        drivetrain.drive_for(FORWARD, F1, MM)
-        drivetrain.turn_for(LEFT, 90, DEGREES)
-        drivetrain.drive_for(FORWARD, F2, MM)
-        drivetrain.turn_for(RIGHT, 90, DEGREES)
-        drivetrain.drive_for(FORWARD, F3, MM, wait=False)
-        AUTOP.broadcast()
-        # Intake on
-        # Top goal
-        wait(8, SECONDS)
+        if bumper_a.pressing():
+            # Right side
+            drivetrain.drive_for(FORWARD, F1, MM)
+            drivetrain.turn_for(RIGHT, 90, DEGREES)
+            drivetrain.drive_for(FORWARD, F2, MM)
+            drivetrain.turn_for(LEFT, 90, DEGREES)
+            drivetrain.drive_for(FORWARD, F3, MM, wait=False)
+            AUTOP.broadcast()
+            # Intake on
+            # Top goal
+            wait(4, SECONDS)
+        else:
+            # Left side
+            drivetrain.drive_for(FORWARD, F1, MM)
+            drivetrain.turn_for(LEFT, 90, DEGREES)
+            drivetrain.drive_for(FORWARD, F2, MM)
+            drivetrain.turn_for(RIGHT, 90, DEGREES)
+            drivetrain.drive_for(FORWARD, F3, MM, wait=False)
+            AUTOP.broadcast()
+            # Intake on
+            # Top goal
+            wait(4, SECONDS)
 
 def onauton_autonomous_0():
     global Automonus, Bottom, Top, O12B, O12F, O12S, AStop, PH, PL, place_Holder, Keep_Code
