12d37c40614ff339836904e07164912ea3c5bbc5
THECODER-01 <noljunk100@gmail.com>
2026-02-07 08:01:15 -0700

    modified:   Code/test/src/test2/src/main.py

--- Code/test/src/test2/src/main.py
+++ Code/test/src/test2/src/main.py
@@ -38,8 +38,8 @@ right_motors = MotorGroup(right_motor_f, right_motor_b)
 # brain_inertial = Inertial(brain.three_wire_port.h)
 # (Optional) Create an Gyro Sensor for a SmartDrive
 Gyro_sensor = Gyro(brain.three_wire_port.h)
-Gyro_sensor.calibrate()
-wait(4, SECONDS) # Wait for calibration to complete
+# Gyro_sensor.calibrate()
+# wait(4, SECONDS) # Wait for calibration to complete
 Gyro_sensor.set_heading(0, DEGREES)
 # Gyro_sensor.quality(100)
 
