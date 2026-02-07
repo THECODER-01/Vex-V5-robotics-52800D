71f97d03277dd043fc4241a71678fd493ddb0af2
THECODER-01 <noljunk100@gmail.com>
2026-02-07 07:37:42 -0700

    modified:   Code/test/src/test2/src/main.py

--- Code/test/src/test2/src/main.py
+++ Code/test/src/test2/src/main.py
@@ -37,9 +37,9 @@ right_motors = MotorGroup(right_motor_f, right_motor_b)
 # (Optional) Create an Inertial Sensor for a SmartDrive
 # brain_inertial = Inertial(brain.three_wire_port.h)
 # (Optional) Create an Gyro Sensor for a SmartDrive
-Gyro_sensor = Gyro(brain.three_wire_port.h)
-# Gyro_sensor.calibrate()
-# Gyro_sensor.set_heading(0, DEGREES)
+#Gyro_sensor = Gyro(brain.three_wire_port.h)
+#Gyro_sensor.calibrate()
+#Gyro_sensor.set_heading(0, DEGREES)
 # Gyro_sensor.quality(100)
 
 # Construct a 4-Motor Drivetrain (SmartDrive is used with an Inertial Sensor)
