413ec2663748f49bce0bcc15d434b4d5fd842aeb
THECODER-01 <noljunk100@gmail.com>
2026-02-23 15:44:58 -0700

    added more

---
+++ Code/test/src/FINAL/skills/dev01/src/prompt.py
@@ -0,0 +1,13 @@
+'''print(
+my first mechanum drive code, using the following values:
+
+Update the code to this please(we are using Mechanum wheels):
+axis 1 (x) - turn (left/right)
+axis 2 - not used
+axis 3 (y) - move up/down (forwards/backwards)
+axis 4 (z) - move left/right
+Q - 3 and 4 (diagonals)
+controller1 Joy stick 1 is 3 and 4
+controller1 Joy stick 2 is 1 and 2
+)
+'''
